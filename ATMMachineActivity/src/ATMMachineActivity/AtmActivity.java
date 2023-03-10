8a81c93bdf6aa79c6f14ee435c157c65e1e44dff
Rafael Espina <espina.rafael1102@gmail.com>
2023-03-10 15:37:26 +0800

    first ever commit

---
+++ ATMMachineActivity/src/ATMMachineActivity/AtmActivity.java
@@ -0,0 +1,10 @@
+package ATMMachineActivity;
+
+public class AtmActivity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String check = "Testing123";
+	}
+
+}
